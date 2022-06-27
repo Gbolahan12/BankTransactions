d1fc6f6cb7326c5d9022934b5bfd96f80fd92f8d
Gbolahan12 <gbolahan.adelabu@gmail.com>
2022-06-27 11:10:13 +0100

    Bank Transactions CRUD

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankTransactions .Models;
+
+namespace BankTransactions.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+              return _context.Transactions != null ? 
+                          View(await _context.Transactions.ToListAsync()) :
+                          Problem("Entity set 'TransactionDbContext.Transactions'  is null.");
+        }
+
+        // GET: Transaction/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transactions = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transactions == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactions);
+        }
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id =0)
+        {
+            if (id == 0)
+            return View(new Transactions());
+            else
+                return View(_context.Transactions.Find(id));
+        }
+
+        // POST: Transaction/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transactions transactions)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transactions.TransactionId == 0)
+                {
+                    transactions.Date = DateTime.Now;
+                    _context.Add(transactions);
+                }
+                else
+                    _context.Update(transactions);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactions);
+        }
+
+        // GET: Transaction/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transactions = await _context.Transactions.FindAsync(id);
+            if (transactions == null)
+            {
+                return NotFound();
+            }
+            return View(transactions);
+        }
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transactions transactions)
+        {
+            if (id != transactions.TransactionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transactions);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionsExists(transactions.TransactionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactions);
+        }
+
+        private bool TransactionsExists(int transactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'TransactionDbContext.Transactions'  is null.");
+            }
+            var transactions = await _context.Transactions.FindAsync(id);
+            if (transactions != null)
+            {
+                _context.Transactions.Remove(transactions);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<Transactions> Transactions { get; set; }
+
+        
+    }
+}
